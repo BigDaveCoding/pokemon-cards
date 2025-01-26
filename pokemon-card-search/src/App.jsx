1ed821591fdfba6088301eaf3aaf3b4d97c3cf34
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 21:38:41 +0000

    added onSearch prop to SearchBar

--- pokemon-card-search/src/App.jsx
+++ pokemon-card-search/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       {/* <h1 className="text-5xl text-red-700">hello is this working?</h1> */}
-      <SearchBar />
+      <SearchBar onSearch={handleData} />
       <DisplayPokemonInfo />
     </>
   )
