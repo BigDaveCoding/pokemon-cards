b144480148dfa59facd98e99cb744be403fa0637
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 21:43:58 +0000

    searchBar.jsx - onSubmit of form runs handleSearch. Passes in value of input from user. uses that value to run onSearch which is passed down from the parent (app.jsx)

--- pokemon-card-search/src/components/search-bar.jsx
+++ pokemon-card-search/src/components/search-bar.jsx
@@ -1,28 +1,25 @@
+import { useState } from 'react'
 import FetchPokeData from '../utilities/poke-api';
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+
+    const [pokemonName, setPokemonName] = useState('');
 
     const handleSearch = (e) => {
         //prevent submit form from refreshing the page
         e.preventDefault()
-        // Get <input> tag containing user search
-        const element = document.querySelector('#pokemon_name')
-        console.log(element)
-        // Take value from the element
-        const pokemon_name = element.value
-        console.log(pokemon_name)
-        // Run FetchPokeData using pokemon_name value.
-        FetchPokeData(pokemon_name)
-        
+        // Run function passed from parent using variable pokemonName
+        onSearch(pokemonName)
+          
     }
 
     return (
         <>
-            <form>
+            <form onSubmit={handleSearch}>
                 <label htmlFor="pokemon_name">Search</label>
-                <input onChange={(e) => console.log(e.target.value)} type="text" id="pokemon_name" name="pokemon_name" placeholder="search for a pokemon"></input>
+                <input onChange={(e) => setPokemonName(e.target.value)} type="text" id="pokemon_name" name="pokemon_name" placeholder="search for a pokemon"></input>
 
-                <input onClick={(e) => handleSearch(e)} type="submit" value="Search" className="border-2 border-green-950 p-1 bg-amber-200" ></input>
+                <input type="submit" value="Search" className="border-2 border-green-950 p-1 bg-amber-200" ></input>
             </form>
         </>
     )
