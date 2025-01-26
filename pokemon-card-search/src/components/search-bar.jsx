758afb632836811322ce44d4494d742a4cb185c9
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 20:34:48 +0000

    created a const to handle the search. finds the input element/store value in variable/use variable in FetchPokeData to return json of that pokemons information

--- pokemon-card-search/src/components/search-bar.jsx
+++ pokemon-card-search/src/components/search-bar.jsx
@@ -1,11 +1,25 @@
 import FetchPokeData from '../utilities/poke-api';
 
 function SearchBar() {
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        const element = document.querySelector('#pokemon_name')
+        console.log(element)
+        const pokemon_name = element.value
+        console.log(pokemon_name)
+        FetchPokeData(pokemon_name)
+        
+    }
+
+
     return (
         <>
             <form>
                 <label htmlFor="pokemon_name">Search</label>
-                <input id="pokemon_name" name="pokemon_name" placeholder="search for a pokemon"></input>
+                <input onChange={(e) => console.log(e.target.value)} type="text" id="pokemon_name" name="pokemon_name" placeholder="search for a pokemon"></input>
+
+                <input onClick={(e) => handleSearch(e)} type="submit" value="Search" className="border-2 border-green-950 p-1 bg-amber-200" ></input>
             </form>
         </>
     )
