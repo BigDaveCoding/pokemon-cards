9bdcf528df7858166a2096bb0b7260bc8cf359a2
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 20:39:08 +0000

    removed test h1 from app.jsx

--- pokemon-card-search/src/App.jsx
+++ pokemon-card-search/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-5xl text-red-700">hello is this working?</h1>
+      {/* <h1 className="text-5xl text-red-700">hello is this working?</h1> */}
       <SearchBar />
     </>
   )
