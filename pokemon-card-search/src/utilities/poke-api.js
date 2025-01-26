078945943e044f00ab7d8441a443c70758a53245
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 20:46:48 +0000

    created variabes storing name and img. console logging these to debug.

--- pokemon-card-search/src/utilities/poke-api.js
+++ pokemon-card-search/src/utilities/poke-api.js
@@ -5,6 +5,10 @@ function FetchPokeData(search) {
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            const pokemon_name = data.name
+            const pokemon_default_img = data.sprites.front_default
+            console.log(pokemon_name)
+            console.log(pokemon_default_img)
         })
 
 }
