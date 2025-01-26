9cdc6c43cedf33de52cb742ea449ae5e35216c36
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 22:08:54 +0000

    storing some more data from the fetch

--- pokemon-card-search/src/utilities/poke-api.js
+++ pokemon-card-search/src/utilities/poke-api.js
@@ -4,12 +4,18 @@ async function FetchPokeData(search) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
     const data = await response.json()
 
+    console.log(data)
+
     const pokemon_name = data.name
     const pokemon_default_img = data.sprites.front_default
+    const pokemon_shiny_img = data.sprites.front_shiny
+    const pokemon_stats_hp = [data.stats[0].stat.name, data.stats[0].base_stat]
 
     return {
         pokemon_name,
-        pokemon_default_img
+        pokemon_default_img,
+        pokemon_shiny_img,
+        pokemon_stats_hp
     }
 
 }
