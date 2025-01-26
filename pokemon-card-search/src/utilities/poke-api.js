23f73d8ebb67e6c2152a866a7c2bef8fbf6feece
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 20:08:21 +0000

    moved fetch into a function so i can add an argument for the search name. I can then use this inside the fetch to display whichever pokemon the user searches for

--- pokemon-card-search/src/utilities/poke-api.js
+++ pokemon-card-search/src/utilities/poke-api.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const fetchData = fetch('https://pokeapi.co/api/v2/pokemon/ivysaur')
+function fetchPokeData(search) {
+    fetch('https://pokeapi.co/api/v2/pokemon/ivysaur')
     .then(response => response.json())
     .then(data => {
         console.log(data)
     })
+
+}
