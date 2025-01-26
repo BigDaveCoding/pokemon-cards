26d3619076450e150d9028df391fed01e6bbb5d4
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 22:12:39 +0000

    added new info to display

--- pokemon-card-search/src/components/display_pokemon_info.jsx
+++ pokemon-card-search/src/components/display_pokemon_info.jsx
@@ -6,6 +6,8 @@ function DisplayPokemonInfo({data}) {
         <>
             <h3>pokemon name: {data.pokemon_name} </h3>
             <img src={data.pokemon_default_img} alt="" />
+            <img src={data.pokemon_shiny_img} alt="" />
+            <p>{data.pokemon_stats_hp[0]}: {data.pokemon_stats_hp[1]}</p>
         </>
     )
 }
