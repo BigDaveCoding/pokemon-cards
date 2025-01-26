8d3dc11ed386e3c1d936cbaa3f17b450753a8e37
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 22:00:08 +0000

    changed FetchPokeDate to a async function. allows me to use await which waits for the response.

--- pokemon-card-search/src/utilities/poke-api.js
+++ pokemon-card-search/src/utilities/poke-api.js
@@ -1,20 +1,16 @@
 "use strict";
 
-function FetchPokeData(search) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            const pokemon_name = data.name
-            const pokemon_default_img = data.sprites.front_default
-            console.log(pokemon_name)
-            console.log(pokemon_default_img)
+async function FetchPokeData(search) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
+    const data = await response.json()
 
-            return {
-                pokemon_name,
-                pokemon_default_img
-            }
-        })
+    const pokemon_name = data.name
+    const pokemon_default_img = data.sprites.front_default
+
+    return {
+        pokemon_name,
+        pokemon_default_img
+    }
 
 }
 
