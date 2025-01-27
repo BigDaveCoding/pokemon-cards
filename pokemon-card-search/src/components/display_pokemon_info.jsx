6b9515a060b65ccd0efdffc131685da25542c25a
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-27 22:06:00 +0000

    wrote function to unpack data sent in an object.

--- pokemon-card-search/src/components/display_pokemon_info.jsx
+++ pokemon-card-search/src/components/display_pokemon_info.jsx
@@ -2,11 +2,23 @@ import FetchPokeData from "../utilities/poke-api"
 
 function DisplayPokemonInfo({data}) {
 
+    const unpackData = (object) => {
+        return Object.entries(object).map(([key, value]) => (
+            <div key={key}>
+                <p>{key}:</p>
+                <p>{value}</p>
+            </div>
+        ));
+    }
+
+    
+
     return (
         <>
             <h3>pokemon name: {data.pokemon_name} </h3>
             <img src={data.pokemon_default_img} alt="" />
             <img src={data.pokemon_shiny_img} alt="" />
+            {unpackData(data.pokemon_stats)}
         </>
     )
 }
