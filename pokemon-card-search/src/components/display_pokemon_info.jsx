008caffbda2543ef7d42cca4f83eb288632ef1fa
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-27 22:14:57 +0000

    styling the display of stats

--- pokemon-card-search/src/components/display_pokemon_info.jsx
+++ pokemon-card-search/src/components/display_pokemon_info.jsx
@@ -4,21 +4,32 @@ function DisplayPokemonInfo({data}) {
 
     const unpackData = (object) => {
         return Object.entries(object).map(([key, value]) => (
-            <div key={key}>
+            <div className="grid grid-cols-2" key={key}>
                 <p>{key}:</p>
                 <p>{value}</p>
             </div>
         ));
     }
 
-    
-
     return (
         <>
-            <h3>pokemon name: {data.pokemon_name} </h3>
-            <img src={data.pokemon_default_img} alt="" />
-            <img src={data.pokemon_shiny_img} alt="" />
-            {unpackData(data.pokemon_stats)}
+            <h3 className="text-center text-4xl uppercase">{data.pokemon_name} </h3>
+
+            <div className="grid grid-cols-2">
+                <div className="flex flex-col justify-center items-center p-2">
+                    <img className="w-full border-2 border-amber-500/50" src={data.pokemon_default_img} alt="" />
+                    <p className="font-bold">Standard</p>
+                </div>
+                <div className="flex flex-col justify-center items-center p-2">
+                    <img className="w-full border-2 border-amber-500/50" src={data.pokemon_shiny_img} alt="" />
+                    <p className="font-bold">Shiny</p>
+                </div>
+            </div>
+
+            <div className="">
+                <h4>Base Stats</h4>
+                {unpackData(data.pokemon_stats)}
+            </div>
         </>
     )
 }
