2c32b8bd63108b69acb57baa769ee7fa2678a881
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-01 21:49:16 +0000

    using a state to set the loading status of both fetch functions. init at false, and setting these bools inside the functions to handle getting the data. if either of them are true it displays a loading message only. if theyre both true, displayPokemonInfo appears.

--- pokemon-card-search/src/App.jsx
+++ pokemon-card-search/src/App.jsx
@@ -10,20 +10,25 @@ import DisplayPokemonInfo from './components/display_pokemon_info'
 function App() {
 
   const [pokemonData, setPokemonData] = useState(null)
+  const [loadingPokeDate, setLoadingPokeData] = useState(false)
 
   const handleData = async (search_name) => {
+    setLoadingPokeData(true)
     const data = await FetchPokeData(search_name)
     setPokemonData(data)
     console.log("poke api data: ", data)
+    setLoadingPokeData(false)
     }
 
   const [tcgPokemonData, setTcgPokemonData] = useState(null)
+  const [loadingTcgData, setLoadingTcgData] = useState(null)
 
   const handleTcgData = async (search_name) => {
+    setLoadingTcgData(true)
     const data = await getPokemonTCGData(search_name)
     console.log("tcgApiData: ", data)
     setTcgPokemonData(data)
-    
+    setLoadingTcgData(false)
   }
 
   return (
@@ -31,7 +36,10 @@ function App() {
       {/* <h1 className="text-5xl text-red-700">hello is this working?</h1> */}
       <div className="">
         <SearchBar onSearch={handleData} onTcgSearch={handleTcgData} />
-        {pokemonData && tcgPokemonData && <DisplayPokemonInfo data={pokemonData} tcgData={tcgPokemonData} />}
+
+        {loadingPokeDate || loadingTcgData && <p className="text-2xl p-2 text-center">Loading Data...</p>}
+
+        {pokemonData && tcgPokemonData && !loadingPokeDate && !loadingTcgData && <DisplayPokemonInfo data={pokemonData} tcgData={tcgPokemonData} />}
       </div>
     </>
   )
