e818c065fb6b4faacc168a4e91adce6a05d1a22c
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-01 21:29:36 +0000

    sorting cards by release date. geting the first card and using the information from the first card

--- pokemon-card-search/src/utilities/pokemon-TCG-api.js
+++ pokemon-card-search/src/utilities/pokemon-TCG-api.js
@@ -4,8 +4,26 @@ async function getPokemonTCGData(query) {
     const response = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${query}`)
     const data = await response.json()
 
-    // console.log(data)
-    return data
+    const all_cards = data.data
+    const sorted_cards = all_cards.sort((cardA, cardB) => {
+        const dateA = cardA.set.releaseDate ? new Date(cardA.set.releaseDate) : new Date(0); 
+        const dateB = cardB.set.releaseDate ? new Date(cardB.set.releaseDate) : new Date(0);  
+  
+        return dateA - dateB;
+    })
+
+    const first_card = sorted_cards[0]
+    const flavor_text = first_card.flavorText
+
+    console.log("first card", first_card)
+    console.log(flavor_text)
+    
+    return {
+        data,
+        first_card,
+        flavor_text
+        
+    }
 }
 
 // getPokemonTCGData("charizard")
