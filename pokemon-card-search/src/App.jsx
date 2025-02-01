372feb23f9e94e3f1130b682183458def3e0f8f0
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-01 21:05:52 +0000

    function to handle tcgData fetch. using serach_name var from SearchBar. new state init at null and being set to tcgData upon fetch from api

--- pokemon-card-search/src/App.jsx
+++ pokemon-card-search/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FetchPokeData from './utilities/poke-api'
+import getPokemonTCGData from './utilities/pokemon-TCG-api'
 import SearchBar from './components/search-bar'
 import DisplayPokemonInfo from './components/display_pokemon_info'
 
@@ -13,14 +14,23 @@ function App() {
   const handleData = async (search_name) => {
     const data = await FetchPokeData(search_name)
     setPokemonData(data)
-    console.log(data)
+    console.log("poke api data: ", data)
     }
+
+  const [tcgPokemonData, setTcgPokemonData] = useState(null)
+
+  const handleTcgData = async (search_name) => {
+    const data = await getPokemonTCGData(search_name)
+    console.log("tcgApiData: ", data)
+    setTcgPokemonData(data)
+    
+  }
 
   return (
     <>
       {/* <h1 className="text-5xl text-red-700">hello is this working?</h1> */}
       <div className="">
-        <SearchBar onSearch={handleData} />
+        <SearchBar onSearch={handleData} onTcgSearch={handleTcgData} />
         {pokemonData && <DisplayPokemonInfo data={pokemonData} />}
       </div>
     </>
