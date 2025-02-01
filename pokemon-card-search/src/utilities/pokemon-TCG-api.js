f751dc630d32ecb67bec324ff6e91b70425e185a
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-01 21:04:26 +0000

    return data instead of console log. this was why I was getting undefined in app.jsx. now is returning corret data

--- pokemon-card-search/src/utilities/pokemon-TCG-api.js
+++ pokemon-card-search/src/utilities/pokemon-TCG-api.js
@@ -4,9 +4,10 @@ async function getPokemonTCGData(query) {
     const response = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${query}`)
     const data = await response.json()
 
-    console.log(data)
+    // console.log(data)
+    return data
 }
 
-getPokemonTCGData("charizard")
+// getPokemonTCGData("charizard")
 
 export default getPokemonTCGData
