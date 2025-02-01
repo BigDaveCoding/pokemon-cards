56bc60a1a7549e8ea6b2ec2329ec6b5df0b3df3c
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-01 18:58:16 +0000

    storing abilities in a const. looping through each ability in variable. fetching data of url stored in ability. Declared an empty object. Adding the fetched data entry for the effect entry into the object whose key is the name of the ability.

--- pokemon-card-search/src/utilities/poke-api.js
+++ pokemon-card-search/src/utilities/poke-api.js
@@ -11,16 +11,36 @@ async function FetchPokeData(search) {
     const pokemon_shiny_img = data.sprites.other["official-artwork"].front_shiny
     const pokemon_stats = {}
 
+    const pokemon_abilities = data.abilities
+    const ability_description = {}
+
+    for (const ability of pokemon_abilities) {
+        // console.log(ability);
+        // console.log(ability.ability.name);
+        // console.log(ability.ability.url);
+
+        const abilityResponse = await fetch(ability.ability.url);
+        const abilityData = await abilityResponse.json();
+
+        // console.log(abilityData);  // Log the ability data
+
+        ability_description[ability.ability.name] = abilityData.effect_entries["1"].effect
+    }
+
+    // console.log(`abilities: ${pokemon_abilities}`)
+    console.log(ability_description)
+
     data.stats.forEach(stat => {
         pokemon_stats[stat.stat.name] = stat.base_stat
     });
-    console.log(pokemon_stats)
+    // console.log(pokemon_stats)
 
     return {
         pokemon_name,
         pokemon_default_img,
         pokemon_shiny_img,
-        pokemon_stats
+        pokemon_stats,
+        ability_description
     }
 
 }
