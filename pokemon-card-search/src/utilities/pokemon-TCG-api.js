ae0367425099a9adecc2a79194c342625f25e231
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-01 22:46:47 +0000

    working on getting the evolution data from pokemon. currently in nested if else statements which needs some work but its returning the evolves to data

--- pokemon-card-search/src/utilities/pokemon-TCG-api.js
+++ pokemon-card-search/src/utilities/pokemon-TCG-api.js
@@ -15,25 +15,51 @@ async function getPokemonTCGData(query) {
     const first_card = sorted_cards[0]
     let flavor_text = ""
 
+    let evolves_to = ""
+
     for (const card of sorted_cards) {
-        console.log(card.flavorText)
+        // console.log(card.flavorText)
         if (card.flavorText === undefined) {
-            console.log("empty flavor text")
+            // console.log("empty flavor text")
         } else {
-            console.log("flovor text exists")
+            // console.log("flovor text exists")
             flavor_text = card.flavorText
             break;
         }
     }
-    
 
-    console.log("first card", first_card)
-    console.log(flavor_text)
+
+    if (!first_card.evolvesTo || first_card.evolvesTo.length === 0) {
+        console.log("This card does not evolve to anything.");
+      } else {
+        console.log("This card evolves to:", first_card.evolvesTo);
+        const evolve = first_card.evolvesTo
+        console.log("length:", evolve.length)
+        if (evolve.length <= 1) {
+            const fetchEvolveResponse = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${evolve}`)
+            const evolveData = await fetchEvolveResponse.json()
+            console.log("evolve data:", evolveData)
+            if (!evolveData.data[0].evolvesTo ){
+                console.log("evolution is final evolution")
+            } else {
+                console.log("evolution has an evolution")
+                const evoEvoResponse = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${evolveData.data[0].evolvesTo[0]}`)
+                const evoEvoData = await evoEvoResponse.json()
+                console.log("evolution of evolution data:", evoEvoData)
+            }
+        } else {
+            console.log("evolution list longer than 1")
+        }
+        
+
+      }
+
     
     return {
         data,
         first_card,
-        flavor_text
+        flavor_text,
+        evolves_to
         
     }
 }
