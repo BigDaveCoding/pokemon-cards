ea59fb30b84bd3eb6fdc4ece086afc3ec2cadbf0
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-03 19:15:39 +0000

    added stying as beyond 768px it didnt match the rest of the document

--- pokemon-card-search/src/components/Footer.jsx
+++ pokemon-card-search/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import CreateText from "./CreateText";
 export default function Footer() {
     return (
         <>
-            <div className="text-slate-100 py-2 border-t-1">
-                <CreateText className="" text="&copy; BigDaveCoding 2025" />
+            <div className="text-slate-100 py-2 border-t-1 max-w-[768px] mx-auto w-full">
+                <CreateText className="px-1" text="&copy; BigDaveCoding 2025" />
             </div>
         </>
     )
