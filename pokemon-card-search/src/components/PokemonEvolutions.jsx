da4dd49bae8eaa00116ec2319bec5ce802655725
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-03 19:16:20 +0000

    added capitalize to styling of createText componenet inside pokemonEvolutions

--- pokemon-card-search/src/components/PokemonEvolutions.jsx
+++ pokemon-card-search/src/components/PokemonEvolutions.jsx
@@ -10,7 +10,7 @@ export default function PokemonEvolutions({image_src_array, name_array, parent_c
                 {image_src_array.map((img_src, index) => (
                     <div className="flex flex-col flex-grow items-center p-2" key={index}>
                         <img className={image_className} src={img_src} alt={name_array[index]} />
-                        <CreateText tag="p" className="text-center text-sm italic font-medium" text={name_array[index]} />
+                        <CreateText tag="p" className="text-center text-sm italic font-medium capitalize" text={name_array[index]} />
                     </div>
                 ))}
             </div>
