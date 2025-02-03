2858e73646b97fccaccf82fccae35c30eba2f3f3
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-03 15:54:10 +0000

    put code insie fragment tags, using creatText componenet to create a title.

--- pokemon-card-search/src/components/PokemonEvolutions.jsx
+++ pokemon-card-search/src/components/PokemonEvolutions.jsx
@@ -3,13 +3,17 @@ import CreateText from "./CreateText";
 
 export default function PokemonEvolutions({image_src_array, name_array, parent_className = '', image_className=''}) {
     return (
-     <div className={parent_className}>
-        {image_src_array.map((img_src, index) => (
-            <div className="flex flex-col flex-grow items-center p-2" key={index}>
-                <img className={image_className} src={img_src} alt={name_array[index]} />
-                <CreateText tag="p" className="text-center text-sm italic font-medium" text={name_array[index]} />
+        <>
+            <CreateText tag="h4" className="text-2xl underline underline-offset-4 p-2" text="Evolutions" />
+            <div className={parent_className}>
+                
+                {image_src_array.map((img_src, index) => (
+                    <div className="flex flex-col flex-grow items-center p-2" key={index}>
+                        <img className={image_className} src={img_src} alt={name_array[index]} />
+                        <CreateText tag="p" className="text-center text-sm italic font-medium" text={name_array[index]} />
+                    </div>
+                ))}
             </div>
-        ))}
-    </div>
+        </>
     )
 }
