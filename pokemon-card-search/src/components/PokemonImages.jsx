28d7c307c77d4a50169c6d7c4efcb6c81e8fccf3
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-03 21:10:06 +0000

    made images in PokemonImages Component width of 11/12 and added a amber border

--- pokemon-card-search/src/components/PokemonImages.jsx
+++ pokemon-card-search/src/components/PokemonImages.jsx
@@ -6,7 +6,7 @@ export default function PokemonImages({images, text}){
         <>
             {images.map((image, index) => (
                 <div key={index} className="flex flex-col justify-center items-center">
-                    <img src={image} alt={`Pokemon {index}`} />
+                    <img className="w-11/12 border-1 border-amber-400" src={image} alt={`Pokemon {index}`} />
                     <CreateText tag="p" className="capitalize font-bold text-center p-2" text={text[index]} />
                 </div>
             ))}
