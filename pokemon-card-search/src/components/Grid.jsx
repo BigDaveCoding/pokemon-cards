1cdeb68710406339f50f600a1abe57836a4d562d
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-03 21:21:07 +0000

    added children argument to Grid component, this goes inside the div. places children inside tags

--- pokemon-card-search/src/components/Grid.jsx
+++ pokemon-card-search/src/components/Grid.jsx
@@ -1,9 +1,9 @@
 
 
-export default function Grid(className="grid grid-cols-1"){
+export default function Grid({className="grid grid-cols-1", children }){
     return (
         <div className={className}>
-
+            {children}
         </div>
     )
 }
