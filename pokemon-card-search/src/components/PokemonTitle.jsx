8a362844ff44182066b9a9f23ea1d5d5eaf7c8af
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-03 15:54:39 +0000

    added styling to the flavor text

--- pokemon-card-search/src/components/PokemonTitle.jsx
+++ pokemon-card-search/src/components/PokemonTitle.jsx
@@ -5,7 +5,7 @@ export default function PokemonTitle({name, text}){
     return (
         <>
             <CreateText tag='h3' className="text-center text-6xl uppercase p-2" text={name} />
-            <CreateText tag="p" className="p-2 text-xl italic" text={text} />
+            <CreateText tag="p" className="p-2 text-xl italic text-center" text={text} />
         </>
     )
 }
