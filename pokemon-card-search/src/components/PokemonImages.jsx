a48d86a0eb0928db14c9d6f1132e2d933c053fce
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-03 14:17:06 +0000

    moved the key to the parent div as i was getting an error saying a child had a missing key.

--- pokemon-card-search/src/components/PokemonImages.jsx
+++ pokemon-card-search/src/components/PokemonImages.jsx
@@ -5,8 +5,8 @@ export default function PokemonImages({images, text}){
     return (
         <>
             {images.map((image, index) => (
-                <div className="flex flex-col justify-center items-center">
-                    <img src={image} key={index} alt={`Pokemon {index}`} />
+                <div key={index} className="flex flex-col justify-center items-center">
+                    <img src={image} alt={`Pokemon {index}`} />
                     <CreateText tag="p" className="capitalize font-bold text-center p-2" text={text[index]} />
                 </div>
             ))}
